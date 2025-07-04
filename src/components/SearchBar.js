765ce888a267056160879e8071eaa4a83f64c938
RaghavKumar67 <107361476+RaghavKumar67@users.noreply.github.com>
2025-07-04 08:05:31 +0530

    Test change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,7 +17,7 @@ function SearchBar({ onSubmit }) {
         className="w-full px-4 py-2 rounded border focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700"
       />
       <button className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
-        Search
+        Search  
       </button>
     </form>
   );
